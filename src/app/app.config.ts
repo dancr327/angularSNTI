d5abd1fdf902f8e2f60ab7224897d261288484a6
dancr327 <cardosa.mx@gmail.com>
2025-05-12 16:32:40 -0600

    agregue prisma y express

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
 // importo provideHttpClient para poder usar el servicio HttpClient, este caso para recuperar los datos de contactos.json
 import { provideHttpClient } from '@angular/common/http';
 
